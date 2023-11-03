3d09eb83adf58d6f654f63decc3fd2ded24a9261
Md. Talal Wasim <md.talalwasim@gmail.com>
2023-11-03 16:08:24 +0600

    1st commit about 'project setup'

---
+++ Take-Note/src/main/java/com/mdtalalwasim/takenote/TakeNoteApplication.java
@@ -0,0 +1,13 @@
+package com.mdtalalwasim.takenote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TakeNoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TakeNoteApplication.class, args);
+	}
+
+}
---
+++ Take-Note/src/test/java/com/mdtalalwasim/takenote/TakeNoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mdtalalwasim.takenote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TakeNoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
